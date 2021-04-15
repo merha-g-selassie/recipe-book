f8920835943df758bc6a8323d5496a4e3b7f207e
merha-g-selassie <merhawi.gs@gmail.com>
2021-04-15 22:39:53 +0200

    test

---
+++ src/app/recipes/recipe.model.ts
@@ -0,0 +1,12 @@
+export class Recipe {
+  public name: string;
+  public description: string;
+  public imagePath: string;
+      private hello : string;
+
+  constructor(name: string, description: string, imagePath: string) {
+    this.name = name;
+    this.description = description;
+    this.imagePath = imagePath;
+  }
+}
